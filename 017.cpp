021ec84feb72d20d1953545fa7a0da1f04ea58a6
tianhaoo <360726539@qq.com>
2022-08-05 16:17:19 +0800

    4

--- 017.cpp
+++ 017.cpp
@@ -11,26 +11,37 @@ using namespace std;
 
 class Solution {
 
+    unordered_map<char, string> phoneMap = {
+            {'2', "abc"},
+            {'3', "def"},
+            {'4', "ghi"},
+            {'5', "jkl"},
+            {'6', "mno"},
+            {'7', "pqrs"},
+            {'8', "tuv"},
+            {'9', "wxyz"}
+    };
+    vector<string> combinations;
 
-    void backtrack(const string& digits, string &combination){
+
+    void backtrack(const string& digits, string &combination, int index){
         if (combination.length() == digits.length()){
-
+            combinations.push_back(combination);
+        }else{
+            char digit = digits[index];
+            const string letters = phoneMap.at(digit);
+            for (const char &letter : letters){
+                combination.push_back(letter);
+                backtrack(digits, combination, index+1);
+                combination.pop_back();
+            }
         }
-
     }
 public:
     vector<string> letterCombinations(string digits) {
-        unordered_map<char, string> phoneMap = {
-                {'2', "abc"}
-                {'3', "def"}
-                {'4', "ghi"}
-                {'5', "jkl"}
-                {'6', "mno"}
-                {'7', "pqrs"}
-                {'8', "tuv"}
-                {'9', "wxyz"}
-        }
-
+        string combination;
+        backtrack(digits, combination, 0);
+        return combinations;
     }
 
 };
